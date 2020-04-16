298768da6348fc7f9d0863233f1a0a5f5e84d4f2
EC2 Default User <ec2-user@ip-172-31-42-30.us-east-2.compute.internal>
2020-04-16 07:16:38 +0000

    Create static_page test

--- test/controllers/static_pages_new_controller_test.rb
+++ test/controllers/static_pages_new_controller_test.rb
@@ -2,5 +2,29 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
-  test "microposts_return" do
+  test "return home page" do
+    get "/"
+    assert_equal 200, @response.status
   end
+  
+  test "return help page" do
+    get "/help"
+    assert_equal 200, @response.status
+  end
+
+  test "return about page" do
+    get "/about"
+    assert_equal 200, @response.status
+  end
+
+  test "return contact page" do
+    get "/contact"
+    assert_equal 200, @response.status
+  end
+  
+  test "return microposts data" do
+    get '/api/v1/static_pages.json'
+    assert_equal 200, @response.status
+    assert (microposts)
+  end
+end
