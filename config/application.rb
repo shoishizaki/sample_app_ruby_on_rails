70664a61c35e07c9a7fcc60bcc0b206ddc9941f1
shoishizaki <shoishizaki1997@gmail.com>
2020-04-22 17:05:00 +0900

    Fixed database timezone

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module SampleApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.time_zone = 'Tokyo' #アプリケーションのタイムゾーン
+    config.active_record.default_timezone = :local #データベースのタイムゾーン
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
