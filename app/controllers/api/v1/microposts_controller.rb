4d3aece18ab03e1593919ebd76773f6e35676486
shoishizaki <shoishizaki1997@gmail.com>
2020-04-29 23:07:06 +0900

    create micropost controller service layer

--- app/controllers/api/v1/microposts_controller.rb
+++ app/controllers/api/v1/microposts_controller.rb
@@ -1,33 +1,40 @@
+require_relative "../../../service/microposts_controller_service"
+
 class Api::V1::MicropostsController < ApiController
 
-def create
-    content = params[:micropost]
-    user_id = params[:user_id]
-    micropost_data = Micropost.new(user_id: user_id, content: content)
+  # 新規投稿
+  def create
+    micropost_data = create_servive_instance.create_post(params[:user_id], params[:micropost])
     if micropost_data.save
       render json: {} ,status: :ok
     else
       render json:{}, status: 500
     end
-end
+  end
 
-# micropost投稿時に渡すuser_idを渡す
-def index
-  user_id = session[:user_id]
-  render json: user_id
-end
+  # micropost投稿時に渡すuser_idを渡す
+  def index
+    user_id = session[:user_id]
+    render json: user_id
+  end
 
-#個人のMicropostを渡す
-def show
-  user_id = params[:id]
-  user_micropost = Micropost.where(user_id: user_id)
-  render json: user_micropost
-end
+  #個人のMicropostを渡す
+  def show
+    render json: create_servive_instance.find_personal_micropst(params[:id])
+  end
 
-def destroy
-  micropost = Micropost.find(params[:id])
-  micropost.destroy
-  render json:{}, status: 200
-end
+  # 投稿記事を消去する。
+  def destroy
+    create_servive_instance.find_micropost(params[:id]).destroy
+    render json:{}, status: 200
+  end
+
+  private
+
+  # サービス層のインスタンスを作成する。
+  def create_servive_instance
+    service = MicropostsControllerService.new
+    return service
+  end
 
 end
---
+++ app/service/microposts_controller_service.rb
@@ -0,0 +1,21 @@
+class MicropostsControllerService
+
+  # 投稿postを作成する。
+  def create_post(user_id, content)
+    post = Micropost.new(user_id: user_id, content: content)
+    return post
+  end
+
+  # 個人の投稿を探す。
+  def find_personal_micropst(user_id)
+    posts = Micropost.where(user_id: user_id)
+    return posts
+  end
+
+  # 指定されたIDの記事を探す。
+  def find_micropost(id)
+    micropost = Micropost.find(id)
+    return micropost
+  end
+
+end
